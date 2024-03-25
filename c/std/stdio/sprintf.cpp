d116358aecb12eb56ed580a239312afbe627bc06
Scott Bennett <scottbennett912@gmail.com>
2024-03-24 21:11:30 -0500

    The printf implementation seems to have a bug when run in wasm (particularly for printing floats/doubles), so disable it until it is fixed

--- c/std/stdio/sprintf.cpp
+++ c/std/stdio/sprintf.cpp
@@ -10,29 +10,29 @@
 
 #include "c/std/__support/CPP/limits.h"
 #include "c/std/__support/arg_list.h"
-#include "c/std/stdio/printf_core/printf_main.h"
-#include "c/std/stdio/printf_core/writer.h"
+// #include "c/std/stdio/printf_core/printf_main.h"
+// #include "c/std/stdio/printf_core/writer.h"
 
 #include <stdarg.h>
 
 namespace __llvm_libc {
 
-LLVM_LIBC_FUNCTION(int, sprintf,
-                   (char *__restrict buffer, const char *__restrict format,
-                    ...)) {
-  va_list vlist;
-  va_start(vlist, format);
-  internal::ArgList args(vlist); // This holder class allows for easier copying
-                                 // and pointer semantics, as well as handling
-                                 // destruction automatically.
-  va_end(vlist);
+// LLVM_LIBC_FUNCTION(int, sprintf,
+//                    (char *__restrict buffer, const char *__restrict format,
+//                     ...)) {
+//   va_list vlist;
+//   va_start(vlist, format);
+//   internal::ArgList args(vlist); // This holder class allows for easier copying
+//                                  // and pointer semantics, as well as handling
+//                                  // destruction automatically.
+//   va_end(vlist);
 
-  printf_core::WriteBuffer wb(buffer, cpp::numeric_limits<size_t>::max());
-  printf_core::Writer writer(&wb);
+//   printf_core::WriteBuffer wb(buffer, cpp::numeric_limits<size_t>::max());
+//   printf_core::Writer writer(&wb);
 
-  int ret_val = printf_core::printf_main(&writer, format, args);
-  wb.buff[wb.buff_cur] = '\0';
-  return ret_val;
-}
+//   int ret_val = printf_core::printf_main(&writer, format, args);
+//   wb.buff[wb.buff_cur] = '\0';
+//   return ret_val;
+// }
 
 } // namespace __llvm_libc
--- c/std/stdio/vsprintf.cpp
+++ c/std/stdio/vsprintf.cpp
@@ -10,24 +10,24 @@
 
 #include "c/std/__support/CPP/limits.h"
 #include "c/std/__support/arg_list.h"
-#include "c/std/stdio/printf_core/printf_main.h"
-#include "c/std/stdio/printf_core/writer.h"
+// #include "c/std/stdio/printf_core/printf_main.h"
+// #include "c/std/stdio/printf_core/writer.h"
 
 #include <stdarg.h>
 
 namespace __llvm_libc {
 
-LLVM_LIBC_FUNCTION(int, vsprintf,
-                   (char *__restrict buffer, const char *__restrict format,
-                    va_list vlist)) {
-  internal::ArgList args(vlist);
+// LLVM_LIBC_FUNCTION(int, vsprintf,
+//                    (char *__restrict buffer, const char *__restrict format,
+//                     va_list vlist)) {
+//   internal::ArgList args(vlist);
 
-  printf_core::WriteBuffer wb(buffer, cpp::numeric_limits<size_t>::max());
-  printf_core::Writer writer(&wb);
+//   printf_core::WriteBuffer wb(buffer, cpp::numeric_limits<size_t>::max());
+//   printf_core::Writer writer(&wb);
 
-  int ret_val = printf_core::printf_main(&writer, format, args);
-  wb.buff[wb.buff_cur] = '\0';
-  return ret_val;
-}
+//   int ret_val = printf_core::printf_main(&writer, format, args);
+//   wb.buff[wb.buff_cur] = '\0';
+//   return ret_val;
+// }
 
 } // namespace __llvm_libc
